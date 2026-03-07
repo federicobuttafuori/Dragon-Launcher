c3d5c23b353565e924b6edbfed69ca3f18799179
Elnix90 <elnix91@gmail.com>
2026-03-07 10:53:52 +0100

    fix: added prefix parameter to toHexWithAlpha.kt, default to true

--- core/common/src/main/java/org/elnix/dragonlauncher/common/utils/colors/toHexWithAlpha.kt
+++ core/common/src/main/java/org/elnix/dragonlauncher/common/utils/colors/toHexWithAlpha.kt
@@ -3,13 +3,6 @@ package org.elnix.dragonlauncher.common.utils.colors
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 
-// --- Utility: convert color → #RRGGBBAA ---
-//fun toHexWithAlpha(color: Color): String {
-//    val argb = color.toArgb()
-//    val rgb = argb and 0xFFFFFF
-//    val alpha = (color.alpha * 255).toInt().coerceIn(0, 255)
-//    return "#${"%06X".format(rgb)}${"%02X".format(alpha)}"
-//}
-
-// --- Utility: convert color → #AARRGGBB ---
-fun Color?.toHexWithAlpha(): String = "#%08X".format(this?.toArgb())
+// ───────────── Utility: convert Color → #AARRGGBB ─────────────
+fun Color?.toHexWithAlpha(prefix: Boolean = true): String =
+    "${if (prefix) "#" else ""}%08X".format(this?.toArgb())
