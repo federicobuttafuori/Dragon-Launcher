67a48226d06e21f8e345d06165c6e7bd7b26f280
Elnix90 <elnix91@proton.me>
2026-04-05 18:46:51 +0200

    fix: refactored icon pack list content with cleaner ui

--- core/ui/src/main/kotlin/org/elnix/dragonlauncher/ui/helpers/iconPackListContent.kt
+++ core/ui/src/main/kotlin/org/elnix/dragonlauncher/ui/helpers/iconPackListContent.kt
@@ -1,14 +1,12 @@
 package org.elnix.dragonlauncher.ui.helpers
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
@@ -18,22 +16,32 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Palette
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import org.elnix.dragonlauncher.common.R
 import org.elnix.dragonlauncher.common.serializables.IconPackInfo
 import org.elnix.dragonlauncher.common.serializables.dummyAppModel
-import org.elnix.dragonlauncher.ui.UiConstants.DragonShape
+import org.elnix.dragonlauncher.ui.components.Spacer
 import org.elnix.dragonlauncher.ui.components.dragon.DragonIconButton
+import org.elnix.dragonlauncher.ui.components.dragon.DragonRow
+import org.elnix.dragonlauncher.ui.helpers.text.TextWithDescription
 import org.elnix.dragonlauncher.ui.remembers.LocalIcons
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 fun LazyListScope.iconPackListContent(
     packs: List<IconPackInfo>,
     selectedPackPackage: String?,
@@ -44,79 +52,84 @@ fun LazyListScope.iconPackListContent(
 ) {
 
     item {
+
+        var isLoading by remember { mutableStateOf(false) }
+
+
+        LaunchedEffect(isLoading) {
+            delay(2000L)
+            isLoading = false
+        }
+
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding( vertical = 8.dp)
         ) {
             Text(
                 text = stringResource(R.string.icon_packs_found, packs.size),
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onBackground
             )
-            // Refresh icon
-            DragonIconButton(
-                onReloadPacks,
-                imageVector = Icons.Default.Refresh,
-                contentDescription = stringResource(R.string.reload)
-            )
+
+            Crossfade(isLoading) {
+                if (it) {
+                    LoadingIndicator()
+                } else {
+                    DragonIconButton(
+                        onClick = {
+                            isLoading = true
+                            onReloadPacks()
+                        },
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = stringResource(R.string.reload)
+                    )
+                }
+            }
         }
     }
 
     items(packs) { pack ->
         val icons = LocalIcons.current
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(DragonShape)
-                .background(MaterialTheme.colorScheme.surface)
-                .clickable { onPackClick(pack) }
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+        DragonRow(
+            { onPackClick(pack) }
         ) {
             val packPkg = pack.packageName
             val packCacheKey = dummyAppModel(packPkg).iconCacheKey.cacheKey
 
-            Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
-                val packIcon = icons[packCacheKey]
+            val packIcon = icons[packCacheKey]
 
-                Box(
-                    Modifier.size(40.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    if (packIcon != null) {
-                        Image(
-                            bitmap = packIcon,
-                            contentDescription = null,
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop
-                        )
-                    } else {
-                        Icon(
-                            Icons.Default.Palette,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                }
-
-                Column {
-                    Text(
-                        text = pack.name,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        style = MaterialTheme.typography.titleMedium
+            Box(
+                Modifier.size(40.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                if (packIcon != null) {
+                    Image(
+                        bitmap = packIcon,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
                     )
-
-                    Text(
-                        pack.packageName,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        style = MaterialTheme.typography.bodySmall
+                } else {
+                    Icon(
+                        Icons.Default.Palette,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface
                     )
                 }
             }
 
-            if (selectedPackPackage == pack.packageName) {
+            Spacer(12.dp)
+
+            TextWithDescription(
+                text = pack.name,
+                description = pack.packageName,
+            )
+
+            Spacer()
+
+            AnimatedVisibility(selectedPackPackage == pack.packageName) {
                 Icon(
                     Icons.Default.Check,
                     contentDescription = null,
@@ -128,39 +141,26 @@ fun LazyListScope.iconPackListContent(
 
     if (showClearOption) {
         item {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(DragonShape)
-                    .background(MaterialTheme.colorScheme.surface)
-                    .clickable { onClearClick() }
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+            DragonRow(
+                { onClearClick() }
             ) {
-                Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.size(40.dp)
-                    )
+                Icon(
+                    Icons.Default.Close,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(12.dp)
 
-                    Column {
-                        Text(
-                            text = stringResource(R.string.default_text),
-                            style = MaterialTheme.typography.titleMedium,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                        Text(
-                            text = stringResource(R.string.use_original_app_icon),
-                            style = MaterialTheme.typography.bodySmall,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                }
 
-                if (selectedPackPackage == null) {
+                TextWithDescription(
+                    text = stringResource(R.string.default_text),
+                    description = stringResource(R.string.use_original_app_icon)
+                )
+
+                Spacer()
+
+                AnimatedVisibility(selectedPackPackage == null) {
                     Icon(
                         Icons.Default.Check,
                         contentDescription = null,
