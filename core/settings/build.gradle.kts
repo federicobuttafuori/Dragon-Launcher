203ed693759332c487bbd3cea2049330a0baa26d
Elnix90 <elnix91@proton.me>
2026-04-06 17:33:25 +0200

    fix: remove useless dependency in settings module

--- core/settings/build.gradle.kts
+++ core/settings/build.gradle.kts
@@ -50,7 +50,6 @@ extensions.configure<LibraryExtension> {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.ui.graphics)
